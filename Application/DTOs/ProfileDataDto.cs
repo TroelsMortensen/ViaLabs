325997e72d9db7501ae88231ba09d818bbbc3ebe
TRMO <tromortensen@hotmail.com>
2022-04-15 06:59:14 +0200

    Starting on a state container for the profile page

---
+++ Application/DTOs/ProfileDataDto.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs.CategoryDTOs;
+using Application.DTOs.GuideDTOs;
+
+namespace Application.DTOs;
+
+public class ProfileDataDto
+{
+    public Dictionary<CategoryDto, ICollection<GuideHeaderDto>> Categories { get; }
+
+    public ProfileDataDto(Dictionary<CategoryDto, ICollection<GuideHeaderDto>> categories)
+    {
+        Categories = categories;
+    }
+}
---
+++ BlazorServerUI/Handlers/ProfileDataManager.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorServerUI.Handlers;
+
+public class ProfileDataManager
+{
+    
+}
