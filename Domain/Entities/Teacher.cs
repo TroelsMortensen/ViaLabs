3b6aa52e1b40cf325d60ac0d899b04f10b74f90c
Troels Mortensen <tromortensen@hotmail.com>
2022-11-28 12:57:40 +0100

    Teacher name validation initially in place. May need more later..?

--- Domain/Entities/Teacher.cs
+++ Domain/Entities/Teacher.cs
@@ -14,17 +14,27 @@ public class Teacher
         Teacher teacher = new(name, new List<Category>());
         Result validationResult = Validate(name);
 
-        return validationResult.HasErrors ? 
-            new(validationResult.Errors) : 
-            new(teacher);
+        return validationResult.HasErrors ? new(validationResult.Errors) : new(teacher);
     }
 
     private static Result Validate(string name)
     {
         Result result = new();
+
         if (string.IsNullOrEmpty(name))
         {
             result.AddError("Teacher.Name", "Name cannot be empty");
+            return result;
+        }
+
+        if (name.Length < 3)
+        {
+            result.AddError("Teacher.Name", "Name must be at least 3 characters");
+        }
+
+        if (name.Length > 15)
+        {
+            result.AddError("Teacher.Name", "Name must be at most 15 characters");
         }
 
         return result;
--- UnitTests/Domain/Entities/TeacherTests.cs
+++ UnitTests/Domain/Entities/TeacherTests.cs
@@ -11,5 +11,32 @@ public class TeacherTests
     {
         Result<Teacher> result = Teacher.Create(null);
         Assert.True(result.HasErrors);
+        Assert.That(result.Errors.First().Attribute, Is.EqualTo("Teacher.Name"));
+    }
+
+    [TestCase("a")]
+    [TestCase("ab")]
+    public void TeacherNameCannotBeLessThan3Letters(string name)
+    {
+        Result<Teacher> result = Teacher.Create(name);
+        Assert.True(result.HasErrors);
+        Assert.That(result.Errors.First().Attribute, Is.EqualTo("Teacher.Name"));
+    }
+
+    [TestCase("abc")]
+    [TestCase("abcefgh")]
+    [TestCase("abcefghijklmnop")]
+    public void TeacherNameIsValidBetween3And15Letters(string name)
+    {
+        Result<Teacher> result = Teacher.Create(name);
+        Assert.False(result.HasErrors);
+    }
+
+    [Test]
+    public void TeacherNameCannotBeMoreThan15Letters()
+    {
+        Result<Teacher> result = Teacher.Create("abcefghijklmnopq");
+        Assert.True(result.HasErrors);
+        Assert.That(result.Errors.First().Attribute, Is.EqualTo("Teacher.Name"));
     }
 }
