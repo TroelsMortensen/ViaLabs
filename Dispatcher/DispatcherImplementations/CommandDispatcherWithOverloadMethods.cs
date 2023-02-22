55b88ff2cc0b8113018188399b35d0d551af867e
Troels Mortensen <tromortensen@hotmail.com>
2023-02-22 11:51:36 +0100

    command dispatcher which uses method overloading. I guess this is the simplest version

---
+++ Dispatcher/DispatcherImplementations/CommandDispatcherWithOverloadMethods.cs
@@ -0,0 +1,23 @@
+﻿using Application.HandlerContracts;
+using Application.UseCases.CategoryUseCases.CategoryCreate;
+using Domain.OperationResult;
+
+namespace Dispatcher.DispatcherImplementations;
+
+public class CommandDispatcherWithOverloadMethods
+{
+    private readonly IServiceProvider serviceProvider;
+
+    public CommandDispatcherWithOverloadMethods(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public Task<Result> Handle(CreateCategoryCommand command)
+    {
+        ICommandHandler<CreateCategoryCommand>? handler = serviceProvider.GetService(typeof(ICommandHandler<CreateCategoryCommand>)) as ICommandHandler<CreateCategoryCommand>;
+        return handler.Handle(command);
+    } 
+    
+    //... more methods with same signature, but different command type argument.
+}
