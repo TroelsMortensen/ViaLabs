fdc13606c860eb4f70f7fc562fdb8b1f29aa23b1
TRMO <tromortensen@hotmail.com>
2023-02-26 13:30:47 +0100

    add more divs for the top row

--- ViewData/ProfileInfo/DTOs/GuideDataVM.cs
+++ ViewData/ProfileInfo/DTOs/GuideDataVM.cs
@@ -1,6 +1,17 @@
-﻿namespace ViewData.ProfileInfo.DTOs;
+﻿using Domain.Entities;
+
+namespace ViewData.ProfileInfo.DTOs;
 
 public class GuideDataVM
 {
+    public string Title { get; set; }
+    public string CategoryName { get; set; }
+    public Guid CategoryId { get; set; }
+
+    public string Description { get; set; }
+    public bool ShowStepNumbers { get; set; }
+
+    public bool IsPublished { get; set; }
     
+    public ICollection<SlideDetails> Slides { get; set; }
 }
